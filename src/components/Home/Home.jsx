022583e9935cd805d47404658c35473e8e821316
Ioan Velev <ioanvelev46@gmail.com>
2024-08-14 14:42:03 +0300

    Create new components

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import { StatsElement } from "../StatsElement/StatsElement";
+import { TaskCard } from "../TaskCard/TaskCard";
+
+export default function Home() {
+  return (
+    <>
+    <TaskCard />
+    <StatsElement />
+    </>
+  )
+}
