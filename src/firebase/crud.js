5afbab8093091e519b2b34209c9430ef11291e95
Ioan Velev <ioanvelev46@gmail.com>
2024-08-15 20:04:13 +0300

    Update crud.js

--- src/firebase/crud.js
+++ src/firebase/crud.js
@@ -1,23 +1,6 @@
-import {
-    collection,
-    getDoc,
-    deleteDoc,
-    doc,
-    updateDoc,
-    getDocs,
-    onSnapshot,
-    addDoc
-} from "firebase/firestore";
-
-import { db } from './firebase';
 
 
-export const createData = async (collectionName, data) => {
-try {
-    const docRef = doc(db, collectionName);
-    await setDoc(docRef, data);
-} catch (error) {
-    
-}
-}
+
+
+
 
